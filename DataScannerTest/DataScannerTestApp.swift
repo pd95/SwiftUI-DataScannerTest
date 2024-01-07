0fe8ac7ff5acefe6279443fa965bdaaa495e31c7
Philipp <pd95@users.noreply.github.com>
2024-01-07 15:47:47 +0100

    Initial Commit

---
+++ DataScannerTest/DataScannerTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataScannerTestApp.swift
+//  DataScannerTest
+//
+//  Created by Philipp on 07.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct DataScannerTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
