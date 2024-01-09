967b4eeaea20a23ccd4a8a55236540401c82140a
Philipp <pd95@users.noreply.github.com>
2024-01-09 21:01:42 +0100

    Remove background and specific foreground colors (which don't work in for darkmode)

--- DataScannerTest/ScanView.swift
+++ DataScannerTest/ScanView.swift
@@ -15,38 +15,31 @@ struct ScanView: View {
     
     var body: some View {
         NavigationView {
-            ZStack {
-                Color.white.ignoresSafeArea()
-                VStack(spacing: 8) {
-                    
-                    Text("Place barcode in the center")
-                        .font(.title3)
-                        .foregroundColor(Color.primary)
-                        .padding(.top, 20)
-                    
-                    Text("The scan will start automatically")
-                        .font(.callout)
-                        .foregroundColor(Color.primary)
-                    
-                    Spacer()
-                    
-                    CenteredSquareView(content: viewInsideScan)
-                        .frame(width: 350, height: 350, alignment: .center)
-                    
-                    Spacer()
-                    
-                    Image(systemName: "qrcode.viewfinder")
-                        .font(.largeTitle)
-                        .foregroundColor(.gray)
-                    
-                    
-                    Text("Tap the icon to run a new scan")
-                        .font(.callout)
-                        .foregroundColor(Color.primary)
-                    
-                    Spacer(minLength: 45)
-                    
-                }
+            VStack(spacing: 8) {
+
+                Text("Place barcode in the center")
+                    .font(.title3)
+                    .padding(.top, 20)
+
+                Text("The scan will start automatically")
+                    .font(.callout)
+
+                Spacer()
+
+                CenteredSquareView(content: viewInsideScan)
+                    .frame(width: 350, height: 350, alignment: .center)
+
+                Spacer()
+
+                Image(systemName: "qrcode.viewfinder")
+                    .font(.largeTitle)
+                    .foregroundColor(.secondary)
+
+
+                Text("Tap the icon to run a new scan")
+                    .font(.callout)
+
+                Spacer(minLength: 45)
             }
             .environmentObject(vm)
             .onAppear {
